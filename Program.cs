62d9cd2a202ac83abab2207f79c4598e430fe0af
amymareflarehr <50854392+amymareflarehr@users.noreply.github.com>
2019-05-21 16:26:15 +1000

    added comment on line 0 describing how to run

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+/**************************************************************************************************** 
+Please copy into new Console App project or run using https://dotnetfiddle.net and pasting the code
+or try https://dotnetfiddle.net/ryXQOj for time and while the link stays alive!
+*****************************************************************************************************/
+
+
+
 using System;
 
 namespace BattleshipStateTracker
